34749e3ae5dde22ffc086fbaca36750340f89d96
sazingakuulie <hanumantdoke143@gmail.com>
2024-05-19 17:05:47 +0530

    routing

--- src/App.js
+++ src/App.js
@@ -1,9 +1,22 @@
-import React from 'react'
+import React from 'react';
+import { BrowserRouter, Route, Routes } from 'react-router-dom'
+import About from './pages/About';
+import Home from './pages/Home';
+import Profile from './pages/Profile';
+import Signin from './pages/Signin';
+import SignUp from './pages/SignUp';
+
 
 export default function App() {
     return (
-        <h1 className='text-red-800'>
-            hiiii
-        </h1>
+        <BrowserRouter>
+            <Routes>
+                <Route path='/' element={<Home />} />
+                <Route path='/sign-in' element={<Signin />} />
+                <Route path='/sign-up' element={<SignUp />} />
+                <Route path='/about' element={<About />} />
+                <Route path='/profile' element={<Profile />} />
+            </Routes>
+        </BrowserRouter>
     )
 }

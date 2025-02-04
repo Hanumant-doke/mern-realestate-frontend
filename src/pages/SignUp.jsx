c01dfb976a6f791e070a6f3266cc3776b5a0b28a
sazingakuulie <hanumantdoke143@gmail.com>
2025-02-04 19:27:52 +0530

    save

--- src/pages/SignUp.jsx
+++ src/pages/SignUp.jsx
@@ -2,8 +2,14 @@ import React from 'react'
 
 function SignUp() {
     return (
-        <div>
-            SignUp
+        <div className='p-3 max-w-lg mx-auto'>
+            <h1 className='text-3xl text-center font-semibold my-7'>Sign Up</h1>
+            <form className='flex flex-col gap-4'>
+                <input type='text' placeholder='username' className='border p-3 rounded-lg' id='username' />
+                <input type='email' placeholder='email' className='border p-3 rounded-lg' id='email' />
+                <input type='password' placeholder='password' className='border p-3 rounded-lg' id='password' />
+                <button className=''>Sign Up</button>
+            </form>
         </div>
     )
 }
